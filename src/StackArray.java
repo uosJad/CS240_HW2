f49384c8bd3b350b7fdc43236ee57de603d4b2f2
Jason Wu <wujason0@gmail.com>
2017-02-06 01:28:22 -0800

    implemented functions for stack array, needs additional testing

---
+++ src/StackArray.java
@@ -0,0 +1,67 @@
+/**
+ *
+ * Array implementation of stack ADT
+ * @author Jason Wu
+ *
+ * @param <T> generic data type
+ */
+public final class StackArray<T> implements StackInterface<T>{
+	private T[] arr;
+	private int numOfItems; // numOfItems - 1 = index of last item in
+	
+	private final static int DEFAULT_SIZE = 10;
+	
+	public StackArray(){
+		this(DEFAULT_SIZE);
+	}
+	
+	public StackArray(int s){
+		
+		@SuppressWarnings("unchecked")
+		T[] temp = (T[])new Object[s];
+		arr = temp;
+		numOfItems= 0;
+	}
+	
+	@Override
+	public void push(T newEntry) {
+		if (numOfItems + 1 <= arr.length){
+			arr[numOfItems] = newEntry;
+			numOfItems++;
+		}
+	}
+
+	@Override
+	public T pop() {
+		T temp = null;
+		if (!isEmpty()){
+			temp = arr[numOfItems - 1];
+			numOfItems--;
+		}
+		return temp;
+	}
+
+	@Override
+	public T peek() {
+		T temp = null;
+		if (!isEmpty())
+			temp = arr[numOfItems - 1];
+		return temp;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		boolean result = false;
+		if (numOfItems <= 0)
+			result = true;
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		for (int i = 0; i < numOfItems; i++){
+			pop();
+		}
+	}
+
+}
