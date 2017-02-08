88b7b50beb2610a72d6fb420e14c736a14d8bf36
Jason Wu <wujason0@gmail.com>
2017-02-08 03:50:52 -0800

    implemented and test queue array

--- src/TestClass.java
+++ src/TestClass.java
@@ -6,11 +6,43 @@ public class TestClass {
 	public static void main(String[] args) {
 		//stackArrayTest();
 		//stackTest();
+		queueTest();
 		
 		
 		
 		
 		
+	}
+	
+	public static void queueTest() {
+		QueueInterface<Integer> tempQ = new QueueArray<>();
+		
+		System.out.print("queue 12 (last 2 are not added): ");
+		for (int i = 0; i < 12; i++){
+			int t = new Random().nextInt(100);
+			tempQ.enqueue(t);
+			System.out.print(t+ " ");
+		}
+		System.out.println();
+		
+		System.out.print("dequeue 5: ");
+		for (int i = 0; i < 5; i++){
+			System.out.print(tempQ.dequeue()+ " ");
+		}
+		System.out.println();
+		
+		System.out.print("queue 5: ");
+		for (int i = 0; i < 5; i++){
+			int t = new Random().nextInt(100);
+			tempQ.enqueue(t);
+			System.out.print(t+ " ");
+		}
+		System.out.println();
+		
+		System.out.print("dequeue all: ");
+		for (int i = 0; i < 10; i++){
+			System.out.print(tempQ.dequeue()+ " ");
+		}
 		
 	}
 	
@@ -19,7 +51,7 @@ public class TestClass {
 		//StackInterface<Integer> tempSA = new StackVector<>();
 		StackInterface<Integer> tempSA = new StackNode<>();
 		
-		System.out.print("push 12 (last 2 are not added to stack due to size limit): ");
+		System.out.print("push 12 (last 2 are not added): ");
 		for (int i = 0; i < 12; i++){
 			int t = new Random().nextInt(100);
 			tempSA.push(t);
